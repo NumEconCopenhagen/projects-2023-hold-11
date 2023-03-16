8fec01a8de4cadc967e742d33d299c394238259f
tbk642 <tbk642@alumni.ku.dk>
2023-03-16 15:55:26 +0100

    Added loop for Household function

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -52,9 +52,21 @@ class HouseholdSpecializationModelClass:
 
         # a. consumption of market goods
         C = par.wM*LM + par.wF*LF
+        
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
+        
+        if par.sigma == 1:
+            H = HM**(1-par.alpha)*HF**par.alpha
+        elif par.sigma == 0:
+            H= np.minimum(HM, HF)
+        else: 
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1))
+
+        
+            
+
+
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
